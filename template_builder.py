b806f16304eb393b88e1c41765556989c9f34448
Adam Block <adam@usa.net>
2020-09-27 22:38:50 -0700

    Add some secrets

--- template_builder.py
+++ template_builder.py
@@ -2,7 +2,7 @@ from jinja2 import FileSystemLoader, Environment
 import uuid
 
 ASTERISK_SERVER_IP = '192.168.19.202'
-SECOND_EXTENSION_ADD = 10
+SECOND_EXTENSION_NUMBER_JUMP = 10
 INTERCOM_EXTENSION = 400
 
 templateLoader = FileSystemLoader(searchpath='./templates')
@@ -12,7 +12,7 @@ sip_template = "sip_custom_post.conf.template"
 template = templateEnv.get_template(sip_template)
 
 sip_template_data = {
-    'second_extension_add': SECOND_EXTENSION_ADD,
+    'second_extension_add': SECOND_EXTENSION_NUMBER_JUMP,
     'intercom_extension': INTERCOM_EXTENSION,
     'base_extensions': [ 101, 102, 103, 104, 105, 106, 107 ]
 }
@@ -28,13 +28,13 @@ sepmac_template_data = [
     {
         'mac_address': 'ac7e8a2bfaf9',
         'base_extension': 101,
-        'base_extension_secret': '',
+        'base_extension_secret': 'MW]euuMj4oZtf7@RKMtFuiqGXfBV9a',
         'phone_label': 'Kitchen'
     },
     {
         'mac_address': '00425ac6268b',
         'base_extension': 102,
-        'base_extension_secret': '',
+        'base_extension_secret': '2txddXTGJtTwFei4bokoGYsz3UsE',
         'phone_label': 'Library'
     }
 ]
@@ -42,7 +42,7 @@ sepmac_template_data = [
 for dataset in sepmac_template_data:
     dataset['guid'] = uuid.uuid1()
     dataset['server_ip'] = ASTERISK_SERVER_IP
-    dataset['second_extension'] = dataset['base_extension'] + SECOND_EXTENSION_ADD
+    dataset['second_extension'] = dataset['base_extension'] + SECOND_EXTENSION_NUMBER_JUMP
     dataset['intercom_extension'] = INTERCOM_EXTENSION
     outputText = template.render(dataset)  # this is where to put args to the template renderer
 
